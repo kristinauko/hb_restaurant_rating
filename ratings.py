27d718f7dffa8bbc74c6e3150fa5e30b8c12fe2c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-14 12:51:25 -0800

    further study completed exercise

--- ratings.py
+++ ratings.py
@@ -1,4 +1,23 @@
 """Restaurant rating lister."""
 
-
 # put your code here
+def format_restaurant_ratings(textfile):
+
+    textfile = open(textfile)
+    restaurant_info_dict = {}
+
+    for line in textfile:
+        line = line.rstrip()
+        name, rating = line.split(":")
+        restaurant_info_dict[name] = rating
+
+    sorted_restaurant_info = sorted(restaurant_info_dict)
+
+    for item in sorted_restaurant_info:
+        rating = restaurant_info_dict[item]
+        print(f'{item} is rated at {rating}.')
+
+
+
+
+format_restaurant_ratings("scores.txt")
